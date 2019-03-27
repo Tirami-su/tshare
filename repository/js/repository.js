3151bd7f31302d8ebdfc71d0b55c0c00ebb5babd
duyan <duyanay@126.com>
2019-03-27 16:42:48 +0800

    修饰首页，实现导航栏的下拉菜单，完成搜索文件和上传文件的界面

---
+++ repository/js/repository.js
@@ -0,0 +1,12 @@
+/**
+ * 切换表单内容
+ */
+function categorySwitch(value) {
+	if (value == '0') {
+		$('#content1').addClass('d-none')
+		$('#content0').removeClass('d-none')
+	} else {
+		$('#content0').addClass('d-none')
+		$('#content1').removeClass('d-none')
+	}
+}
