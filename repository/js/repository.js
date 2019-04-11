de582f31c50e73d996a8b0be89f0a190cccf6ee8
duyan <duyanay@126.com>
2019-04-11 22:51:04 +0800

    拆分资料板块的html和js

--- repository/js/repository.js
+++ repository/js/repository.js
@@ -1,5 +1,5 @@
 /**
- * 搜索框回车事件
+ * 导航栏搜索框和中心搜索框回车事件
  */
 function inputSearch(key) {
 	if (event.keyCode == 13) {
@@ -7,137 +7,8 @@ function inputSearch(key) {
 		if (key == '')
 			event.preventDefault()
 		else {
-			// 全局变量初始化
-			if (typeof globalMode == 'undefined')
-				globalMode = $('#search-mode').prop('checked') ? 0 : 1
-			if (typeof globalSort == 'undefined')
-				globalSort = $('input[name=sort]:checked').val()
-			curPage = 1
-			// 搜索
-			searchFile(key, true)
+			// 跳转到list.html并传值
+			location = "list.html?key="+encodeURI(key)
 		}
 	}
-}
-
-/**
- * 搜索资料
- */
-function searchFile(key, change = false) {
-	// 请求服务器
-	$.get('api/search_file.php', {
-		key: key,
-		mode: globalMode,
-		sort: globalSort,
-		page: curPage
-	}, res => {
-		fileList(key, res)
-	}, "json")
-	// 局部页面改变
-	if (change) {
-		$('#search-form-mid').remove()
-		$('#navbar-search').val(key)
-	}
-	// 等待动画
-	$('.main').waitMe({
-		effect: 'rotateplane',
-		// text:'正在搜索...',
-		bg: 'rgba(255,255,255,0.8)',
-		color: ['#aaa', '#666'], //前者是字体颜色，后者是动画颜色
-		fontSize: '16px'
-	})
-}
-
-/**
- * 显示搜索结果
- */
-function fileList(key, res) {
-	// 隐藏等待动画，清除找不到的提示
-	$('.main').waitMe('hide')
-	$('#nofound').remove()
-	// 填模版
-	var cellHtml = ''
-	if (res.code == 0) {
-		cellHtml = template('template-nofound', {keyword: key})
-		$('#result').addClass('d-none')
-		$('.main').append(cellHtml)
-	} else {
-		$('#result').removeClass('d-none')
-		// 填cell模版
-		var data = res.data
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].contents == '')
-				cellHtml += template('template-file', data[i])
-			else
-				cellHtml += template('template-folder', data[i])
-		}
-		// 清空列表，重新添加cell
-		$('#file-list').empty().append(cellHtml)
-
-		//填分页器模板
-		pageHtml = '<li id="page-prev" class="page-item disabled"><a class="page-link" href="#" onclick="prevPage()">上一页</a></li>'
-		if (res.amount > 10) {
-			totalpages = Meth.ceil(res.amount / 10)
-			// 如果页数太多，只显示当前页前后9页
-			if (totalpages > 9) {
-				var start = curPage - 4 > 1 ? curPage - 4 : 1
-				var end = curPage + 4 < totalpages ? curPage + 4 : totalpages
-				for (var i = start; i <= end; i++)
-					pageHtml += template('template-page', {num: i})
-			} else
-				for (var i = 1; i <= totalpages; i++)
-					pageHtml += template('template-page', {num: i})
-		} else
-			pageHtml += '<li id="page-1" class="page-item"><a class="page-link" href="#" onclick="toPage(this.text)">1</a></li>'
-		pageHtml += '<li id="page-next" class="page-item disabled"><a class="page-link" href="#" onclick="nextPage()">下一页</a></li>'
-		// 清空分页器，重新添加页项
-		$('#pagination').empty().append(pageHtml)
-		// 设置按钮状态
-		if (curPage != 1)
-			$('#page-prev').removeClass('disabled')
-		if (curPage < totalpages)
-			$('#page-next').removeClass('disabled')
-		$('#page-' + curPage).addClass('active')
-	}
-}
-
-/**
- * 切换搜索模式，可分或不可分
- */
-function modeSwitch() {
-	globalMode = $('#search-mode').prop('checked') ? 0 : 1,
-		curPage = 1
-	searchFile($('#navbar-search').val())
-}
-
-/**
- * 	切换排序方式
- */
-function sortSwitch(sort) {
-	globalSort = sort
-	curPage = 1
-	searchFile($('#navbar-search').val())
-}
-
-/**
- * 上一页
- */
-function prevPage() {
-	curPage -= 1
-	searchFile($('#navbar-search').val())
-}
-
-/**
- * 下一页
- */
-function nextPage() {
-	curPage += 1
-	searchFile($('#navbar-search').val())
-}
-
-/**
- * 跳转到某一页
- */
-function toPage(page) {
-	curPage = page
-	searchFile($('#navbar-search').val())
 }
