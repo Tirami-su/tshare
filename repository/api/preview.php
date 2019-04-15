766e381fb27908c7c9496c33cd73181c6fe5506a
fuhao <1518542802@qq.com>
2019-04-15 14:29:31 +0800

    完善搜索；完成预览PDF的生成

--- repository/api/preview.php
+++ repository/api/preview.php
@@ -1,5 +1,9 @@
 <?php
 /*********** 文件预览 ***********/
+/**
+ * 接收参数，如果是可以预览的文件，则转化为一个指定页数的pdf
+ */
+
 include_once("../../lib/PDF.php");
 include_once("../../lib/FileProcess.php");
 include_once("../../lib/Image.php");
@@ -22,57 +26,30 @@ if(!file_exists("../../".$srcFile)) {
 	exit;
 }
 
-$destFile = "repository/temp/".$objFile;	// 预览文件的保存路径（相对于网站根目录的路径）
-if(!file_exists("../../".$destFile)) {
-	FileProcess::createFolder("../../".$destFile);
+$destFile = "repository/temp/".$filename."-预览.pdf";	// 预览文件的保存路径（相对于网站根目录的路径）
+$parentFile = dirname(dirname(__FILE__)."/../../".$destFile);		// 预览文件的父文件夹
+if(!file_exists($parentFile)) {
+	// 逐级创建父目录
+	FileProcess::createFolder($parentFile);
 }
 
 if(in_array($ext, $valid)) {
-	if(FileProcess::getFolderSize("../../".$destFile) == 0) {
+	if(!file_exists("../temp/".$objFile.".svg")) {
 		// 如果没有预览文件，则需要新建预览文件
 		if($ext == "pdf") {
-			// 如果源文件就是pdf，则先进行拆分，再转换为png图片
-			$tempFile = $destFile."/预览.pdf";
-			PDF::PDFSplit($srcFile, $tempFile, 10);	// 拆成一个小的pdf进行预览
-			PDF::Pdf2Png($tempFile, $destFile);		// 将小pdf转为png图片
-			unlink("../../".$tempFile);				// 删除小的预览pdf
+			// 如果源文件就是pdf，则只需要进行拆分
+			PDF::PDFSplit($srcFile, $destFile, 10);	// 拆成一个小的pdf进行预览
 		} else {
 			// 源文件不是pdf，需要先转为pdf再拆分，然后转为png图片
 			$pdfTemp = "repository/temp/".$filename.".pdf";		// 大pdf路径
 			PDF::Word2Pdf($srcFile, $pdfTemp);			// 转成pdf
-
-			$tempFile = $destFile."/预览.pdf";		// 小pdf路径
-			PDF::PDFSplit($pdfTemp, $tempFile, 10);	// 拆成一个小的pdf进行预览
-
-			PDF::Pdf2Png($tempFile, $destFile);		// 小pdf转为png图片
+			PDF::PDFSplit($pdfTemp, $destFile, 10);	// 拆成一个小的pdf进行预览
 			unlink("../../" . $pdfTemp);			// 删除大pdf文件
-			unlink("../../" . $tempFile);			// 删除小pdf文件
 		}
-		
-		// 将多个png图片合并为一张图片
-		$height = merge("../../".$destFile);
-		FileProcess::delDirectory("../../".$destFile);
 	}
-	echo json_encode(['code' => 1, 'msg' => $height]);
+	echo json_encode(['code' => 1, 'msg' => '预览PDF生成成功']);
 } else {
 	// 不能预览
 	echo json_encode(['code' => 0, 'msg' => "文件格式不支持预览"]);
-}
-
-function merge(String $dir) {
-	$arr = array();
-	$index = 0;
-
-	$handle = opendir($dir);
-	while(($filename=readdir($handle)) !== false) {
-		if($filename != ".." && $filename != ".") {
-			$arr[$index++] = $dir."/".$filename;
-		}
-	}
-	closedir($handle);
-
-	$info = getimagesize($arr[0]);
-	Image::merge($arr, $dir.".png");
-	return $info[1];
 }
 ?>
